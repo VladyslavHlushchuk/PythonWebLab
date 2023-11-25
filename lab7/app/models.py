1cc6026d0beac19def30687b4e5873cc65e08b23
VladyslavHlushchuk <95494733+VladyslavHlushchuk@users.noreply.github.com>
2023-11-25 21:52:38 +0200

    Lab 7

---
+++ lab7/app/models.py
@@ -0,0 +1,32 @@
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Todo(db.Model):
+    __tablename__ = "todo"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+
+    def __repr__(self):
+        return f"{self.id} -- {self.username} -- {self.email}"
+
+    def checkPassword(self, pwd):
+        return check_password_hash(self.password, pwd)
+
